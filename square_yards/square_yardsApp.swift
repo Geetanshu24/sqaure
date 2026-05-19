44bfb447cf84dc9b1b9b7cb2d4996ccd9ebf0e8e
valuehubconsultancypvtltd-hash <valuehubconsultancypvtltd@gmail.com>
2026-05-19 15:37:04 +0530

    Initial Commit

---
+++ square_yards/square_yardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  square_yardsApp.swift
+//  square_yards
+//
+//  Created by Geetanshu Bansal on 19/05/26.
+//
+
+import SwiftUI
+
+@main
+struct square_yardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
