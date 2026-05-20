982bef2fe0340bbfbfc5b94b96fd6ece999ff630
valuehubconsultancypvtltd-hash <valuehubconsultancypvtltd@gmail.com>
2026-05-20 20:25:52 +0530

    fourth commit

--- square_yards/square_yardsApp.swift
+++ square_yards/square_yardsApp.swift
@@ -1,10 +1,25 @@
+import AVFoundation
 import SwiftUI
 
 @main
 struct square_yardsApp: App {
+    init() {
+        configureAudioSession()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+
+    private func configureAudioSession() {
+        do {
+            let session = AVAudioSession.sharedInstance()
+            try session.setCategory(.playback, mode: .moviePlayback, options: [])
+            try session.setActive(true)
+        } catch {
+            assertionFailure("Failed to configure audio session: \(error.localizedDescription)")
         }
     }
 }
